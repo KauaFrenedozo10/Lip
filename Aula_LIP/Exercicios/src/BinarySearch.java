77244b189fac72681b1a21dab1bfd17ed74860af
KauaFrenedozo10 <kfrenedoso@gmail.com>
2026-04-27 22:00:56 -0300

    BinarySearch

---
+++ Aula_LIP/Exercicios/src/BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        
+        int myNumber = 14;
+
+        for(int i = 0; i < 10; i++) {
+            int numeroAl = (int) (1 + Math.random() * 1000);
+             System.out.println(numeroAl);
+
+            int [] myArray = { numeroAl};
+
+            int left = 0;
+            int right = myArray.length - 1;
+
+             while (left <= right) {
+                int mid = (left + right) / 2;
+        }
+
+        }
+        
+       
+        
+        
+       
+
+       
+
+
+    }
+}
